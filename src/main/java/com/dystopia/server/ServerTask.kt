bf078389255a61dc78a5169593f3aacdf0abff5f
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2018-02-04 20:49:09 +0300

    fix maven + dependencies

--- src/main/java/com/dystopia/server/ServerTask.kt
+++ src/main/java/com/dystopia/server/ServerTask.kt
@@ -1,10 +1,24 @@
 package com.dystopia.server
 
 import com.dystopia.message.Messages
-import java.net.Socket
 
-class ServerTask(private val socket: Socket): Runnable {
+import java.io.IOException
+import java.net.Socket
+import java.util.logging.Level
+import java.util.logging.Logger
+
+class ServerTask(private val mySocket: Socket) : Runnable {
+
     override fun run() {
-        Messages.Task.parseDelimitedFrom(socket.getInputStream())
+        try {
+            Messages.Task.parseDelimitedFrom(mySocket.getInputStream())
+        } catch (e: IOException) {
+            LOG.log(Level.WARNING, e.message)
+        }
+
+    }
+
+    companion object {
+        private val LOG = Logger.getLogger("ServerTask")
     }
 }
